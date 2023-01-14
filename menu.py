bb415579df8967a18adeda36588f2a7e7159b99b
Martin Tykhelle <martintykhelle@gmail.com>
2023-01-14 18:42:39 +0100

    Added classes

--- menu.py
+++ menu.py
@@ -5,7 +5,167 @@ import keypad
 encoder = rotaryio.IncrementalEncoder(board.GP10, board.GP11)
 lastPosition = None
 
-#rewrite
+class Options:
+    def __init__(self, options: list):
+        self.options = []
+        for option in options:
+            if(isinstance(option,tuple)):
+                self.options.append({"title":option[0], "value":option[1]})
+            elif(isinstance(option,str)):
+                self.options.append({"title":option, "value":option.lower().replace(" ", "_")})              
+
+class Selection:
+    def __init__(self, title:str, options, name:str=None,value=None):
+        self.optionIdx = 0
+        self.title = title
+        if name:
+            self.name = name
+        else:
+            self.name = self.title.lower().replace(" ", "_")
+        
+        if(isinstance(options,Options)):
+            self.options = options.options
+            if value is not None:
+                self.value = value
+            else:
+                self.value = self.options[0]["value"]
+        elif(isinstance(options,range)):
+            self.options = list(options)
+            if value is not None:
+                self.value = value
+                self.optionIdx = self.options.index(value)
+            else:
+                self.value = self.options[0]
+    
+    def getValue(self):
+        return self.value
+
+    def setValue(self,value):
+        self.value = value
+    
+    def setCurrentOption(self):
+        if(isinstance(self.options,Options)):    
+            self.value = self.options[self.optionIdx]["value"]
+        else:
+            self.value = self.options[self.optionIdx]
+
+    def nextOption(self):
+        self.optionIdx = self.optionIdx +1
+        if(self.optionIdx >= len(self.options)):
+            self.optionIdx = 0 
+
+    def prevOption(self):
+        self.optionIdx = self.optionIdx -1                
+        if(self.optionIdx < 0):
+            self.optionIdx = len(self.options) -1
+    
+    def getCurrentOption(self):
+        return self.options[self.optionIdx]
+    
+    def getCurrentValueTitle(self):
+        if(isinstance(self.options,Options)):    
+            filteredList = list(filter(lambda val: val["value"]==self.value,self.options))[0]
+            return filteredList["title"]
+        else:
+            return self.value
+
+
+  
+class SubMenu:
+    def __init__(self,title:str,name:str=None):
+        self.title = title
+        self.selections = []
+        self.selectionIdx = 0
+        if name:
+            self.name
+        else:
+            self.name =self.title.lower().replace(" ","_")
+    
+    def addSelection(self,selection:Selection):
+        self.selections.append(selection)
+    def getCurrentSelection(self):
+        return self.selections[self.selectionIdx]
+
+    def nextSelection(self):
+        self.selectionIdx = self.selectionIdx +1
+        if(self.selectionIdx >= len(self.selections)):
+            self.selectionIdx = 0 
+
+    def prevSelection(self):
+        self.selectionIdx = self.selectionIdx -1                
+        if(self.selectionIdx < 0):
+            self.selectionIdx = len(self.selections) -1
+
+
+
+class Menu:
+    def __init__(self):
+        self.submenus = []
+        self.submenuIdx = 0
+    def addSubMenu(self, submenu:SubMenu):
+        self.submenus.append(submenu)  
+    def nextSubMenu(self):
+        self.submenuIdx = self.submenuIdx +1
+        if(self.submenuIdx >= len(self.submenus)):
+            self.submenuIdx = 0 
+
+    def prevSubMenu(self):
+        self.submenuIdx = self.submenuIdx -1                
+        if(self.submenuIdx < 0):
+            self.submenuIdx = len(self.submenus) -1
+
+
+    
+    
+
+channel = SubMenu("Channel 1")
+channel.addSelection(Selection("Type",Options(["Sine","Square","Triangle"])))
+channel.addSelection(Selection("Transpose",range(-12,12,1),value=0))
+
+channel.nextSelection()
+
+print(channel.title)
+print(channel.getCurrentSelection().title)
+print(channel.getCurrentSelection().getCurrentValueTitle())
+
+channel.getCurrentSelection().nextOption()
+channel.getCurrentSelection().setCurrentOption()
+print(channel.getCurrentSelection().getCurrentValueTitle())
+
+channel.getCurrentSelection().nextOption()
+channel.getCurrentSelection().setCurrentOption()
+
+print(channel.getCurrentSelection().getCurrentValueTitle())
+
+channel.getCurrentSelection().nextOption()
+channel.getCurrentSelection().setCurrentOption()
+print(channel.getCurrentSelection().getCurrentValueTitle())
+
+
+
+channel.getCurrentSelection().prevOption()
+channel.getCurrentSelection().setCurrentOption()
+print(channel.getCurrentSelection().getCurrentValueTitle())
+
+channel.getCurrentSelection().prevOption()
+channel.getCurrentSelection().setCurrentOption()
+
+print(channel.getCurrentSelection().getCurrentValueTitle())
+
+channel.getCurrentSelection().prevOption()
+channel.getCurrentSelection().setCurrentOption()
+print(channel.getCurrentSelection().getCurrentValueTitle())
+
+channel.getCurrentSelection().prevOption()
+channel.getCurrentSelection().setCurrentOption()
+print(channel.getCurrentSelection().getCurrentValueTitle())
+
+
+
+
+
+
+
 menu = {"channels":[]}
 types = [{"title":"Sine", "value":"sine"},
          {"title":"Square", "value":"square"},
